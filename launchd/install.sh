50e226ec604566912bcc1e619b89a6069c086023
Severin Smith <severin@smallbutdigital.com>
2025-10-05 21:42:06 -0400

    Unnecssary bash script improvements

--- launchd/install.sh
+++ launchd/install.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
+script_path=$(dirname $(realpath "$0"))
+project_path=$(dirname "$script_path")
+cd $script_path
+
+./uninstall.sh
 
 declare -a services=("ca.parquette.lighting.openstagecontrol" "ca.parquette.lighting.server")
 
 for service_name in "${services[@]}"
 do
-    if [ -f ~/Library/LaunchAgents/$service_name.plist ]; then
-        echo "Unload and delete old plist ~/Library/LaunchAgents/$service_name.plist"
-        launchctl bootout gui/$(id -u) ~/Library/LaunchAgents/$service_name.plist 2> /dev/null
-        rm ~/Library/LaunchAgents/$service_name.plist
-    fi
+    # if [ -f ~/Library/LaunchAgents/$service_name.plist ]; then
+    #     echo "Unload and delete old plist ~/Library/LaunchAgents/$service_name.plist"
+    #     launchctl bootout gui/$(id -u) ~/Library/LaunchAgents/$service_name.plist 2> /dev/null
+    #     rm ~/Library/LaunchAgents/$service_name.plist
+    # fi
     echo "Creating ~/Library/LaunchAgents/$service_name.plist"
     contents="$(cat $service_name.plist)"
     output=$contents
     output="${contents//\/Users\/user/$HOME}"
-    output="${output//\/ProjectPath/$(dirname $(dirname $(realpath $0)))}"
+    output="${output//\/ProjectPath/$project_path}"
     echo "$output" >> ~/Library/LaunchAgents/$service_name.plist
 done
 
