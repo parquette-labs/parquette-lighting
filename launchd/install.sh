d22cc6b3bdf62f2da0d82af893b8522df905c0b0
Severin Smith <severin@smallbutdigital.com>
2025-10-05 21:30:37 -0400

    Basic launchd setup

---
+++ launchd/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+declare -a services=("ca.parquette.lighting.openstagecontrol" "ca.parquette.lighting.server")
+
+for service_name in "${services[@]}"
+do
+    if [ -f ~/Library/LaunchAgents/$service_name.plist ]; then
+        echo "Unload and delete old plist ~/Library/LaunchAgents/$service_name.plist"
+        launchctl bootout gui/$(id -u) ~/Library/LaunchAgents/$service_name.plist 2> /dev/null
+        rm ~/Library/LaunchAgents/$service_name.plist
+    fi
+    echo "Creating ~/Library/LaunchAgents/$service_name.plist"
+    contents="$(cat $service_name.plist)"
+    output=$contents
+    output="${contents//\/Users\/user/$HOME}"
+    output="${output//\/ProjectPath/$(dirname $(dirname $(realpath $0)))}"
+    echo "$output" >> ~/Library/LaunchAgents/$service_name.plist
+done
+
+read -p "Do you want to load and start services now? (y/n) " yn
+case $yn in
+    [Yy]* )
+    	for service_name in "${services[@]}"
+    	do
+    		launchctl bootstrap gui/$(id -u) ~/Library/LaunchAgents/$service_name.plist
+    		launchctl kickstart gui/$(id -u)/$service_name
+    	done
+esac
