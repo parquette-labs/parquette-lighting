6f385de3c97aea5d170a48bffc3894f89f100167
Severin Smith <severin@smallbutdigital.com>
2025-10-15 02:38:41 -0400

    Working all input modules

--- esp/controller_base/src/controller_base.ino
+++ esp/controller_base/src/controller_base.ino
@@ -34,7 +34,7 @@ int32_t rot_wheel_position;
 #define QWIIC_ADDR_JOYSTICK 0x21
 JOYSTICK joystick;
 
-#define QWIIC_ADDR_KEYPAD 0x20
+#define QWIIC_ADDR_KEYPAD 0x4B
 KEYPAD keypad;
 
 uint32_t Wheel(byte WheelPos) {
@@ -147,26 +147,33 @@ void loop() {
 	}
 
 	if (! rot_wheel.digitalRead(ROT_WHEEL_SWITCH_UP)) {
-	  Serial.println("UP pressed!");
+		Serial.println("UP pressed!");
 	}
 	if (! rot_wheel.digitalRead(ROT_WHEEL_SWITCH_DOWN)) {
-	  Serial.println("DOWN pressed!");
+		Serial.println("DOWN pressed!");
+		rgbLedWrite(RGB_BUILTIN, RGB_BRIGHTNESS, 0, 0);  // Red
+		delay(250);
+		rgbLedWrite(RGB_BUILTIN, 0, RGB_BRIGHTNESS, 0);  // Green
+		delay(250);
+		rgbLedWrite(RGB_BUILTIN, 0, 0, RGB_BRIGHTNESS);  // Blue
+		delay(250);
+		rgbLedWrite(RGB_BUILTIN, 0, 0, 0);  // Off / black
 	}
 	if (! rot_wheel.digitalRead(ROT_WHEEL_SWITCH_SELECT)) {
-	  Serial.println("SELECT pressed!");
+		Serial.println("SELECT pressed!");
 	}
 	if (! rot_wheel.digitalRead(ROT_WHEEL_SWITCH_LEFT)) {
-	  Serial.println("LEFT pressed!");
+		Serial.println("LEFT pressed!");
 	}
 	if (! rot_wheel.digitalRead(ROT_WHEEL_SWITCH_RIGHT)) {
-	  Serial.println("RIGHT pressed!");
+		Serial.println("RIGHT pressed!");
 	}
 
 	new_position = rot_wheel.getEncoderPosition();
 	// did we move around?
 	if (rot_wheel_position != new_position) {
-	  Serial.println(new_position);         // display new position
-	  rot_wheel_position = new_position;      // and save for next round
+		Serial.println(new_position);         // display new position
+		rot_wheel_position = new_position;      // and save for next round
 	}
 
 	uint16_t x = joystick.getHorizontal();
@@ -208,45 +215,3 @@ void loop() {
 	// don't overwhelm serial port
 	delay(10);
 }
-
-// ---- //
-
-// /*
-//   BlinkRGB
-
-//   Demonstrates usage of onboard RGB LED on some ESP dev boards.
-
-//   Calling digitalWrite(RGB_BUILTIN, HIGH) will use hidden RGB driver.
-
-//   RGBLedWrite demonstrates control of each channel:
-//   void rgbLedWrite(uint8_t pin, uint8_t red_val, uint8_t green_val, uint8_t blue_val)
-
-//   WARNING: After using digitalWrite to drive RGB LED it will be impossible to drive the same pin
-//     with normal HIGH/LOW level
-// */
-// //#define RGB_BRIGHTNESS 64 // Change white brightness (max 255)
-
-// // the setup function runs once when you press reset or power the board
-
-// void setup() {
-//   // No need to initialize the RGB LED
-// }
-
-// // the loop function runs over and over again forever
-// void loop() {
-// #ifdef RGB_BUILTIN
-//   digitalWrite(RGB_BUILTIN, HIGH);  // Turn the RGB LED white
-//   delay(1000);
-//   digitalWrite(RGB_BUILTIN, LOW);  // Turn the RGB LED off
-//   delay(1000);
-
-//   rgbLedWrite(RGB_BUILTIN, RGB_BRIGHTNESS, 0, 0);  // Red
-//   delay(1000);
-//   rgbLedWrite(RGB_BUILTIN, 0, RGB_BRIGHTNESS, 0);  // Green
-//   delay(1000);
-//   rgbLedWrite(RGB_BUILTIN, 0, 0, RGB_BRIGHTNESS);  // Blue
-//   delay(1000);
-//   rgbLedWrite(RGB_BUILTIN, 0, 0, 0);  // Off / black
-//   delay(1000);
-// #endif
-// }
