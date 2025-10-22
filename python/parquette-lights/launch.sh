96129fbdfcd1daf7817aa1f4464b5cb378a556fd
Severin Smith <severin@smallbutdigital.com>
2025-10-22 18:36:49 -0400

    Make the launch not need a reliable profile

--- python/parquette-lights/launch.sh
+++ python/parquette-lights/launch.sh
@@ -1,2 +1,6 @@
-#!/bin/bash
+#!/bin/zsh
+export PATH="/usr/local/sbin:$PATH"
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - zsh)"
 poetry run server --boot-art-net --local-ip 0.0.0.0
