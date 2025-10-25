88076f245f0ef8bdf592fa2ec3fb33104b27249f
Severin Smith <severin@smallbutdigital.com>
2025-10-25 15:56:04 -0400

    Update to include default state update

--- python/parquette-lights/src/parquette/lights/server.py
+++ python/parquette-lights/src/parquette/lights/server.py
@@ -439,6 +439,11 @@ def run(
         presets.select("booth", "Off")
         presets.select("washes", "Off")
         presets.select("spots_light", "Off")
+        mixer.reds_master = 0
+        mixer.spots_master = 0
+        mixer.washes_master = 0
+        mixer.booth_master = 0
+        mixer.plants_master = 0
         mixer.setChannelLevel("sodium", 0)
 
     def house_lights():
@@ -447,6 +452,11 @@ def run(
         presets.select("booth", "Static")
         presets.select("washes", "Static")
         presets.select("spots_light", "Off")
+        mixer.reds_master = 1
+        mixer.spots_master = 1
+        mixer.washes_master = 1
+        mixer.booth_master = 1
+        mixer.plants_master = 1
         mixer.setChannelLevel("sodium", 255)
 
     osc.dispatcher.map("/all_black", lambda addr, args: all_black())
