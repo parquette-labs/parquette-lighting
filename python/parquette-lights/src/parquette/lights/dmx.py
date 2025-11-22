39260af4e29a8486a042135e18a63e2773599ebd
Severin Smith <severin@smallbutdigital.com>
2025-11-22 16:27:55 -0500

    More debug

--- python/parquette-lights/src/parquette/lights/dmx.py
+++ python/parquette-lights/src/parquette/lights/dmx.py
@@ -163,7 +163,7 @@ class DMXManager(object):
                     self.close()
 
     def submit(self) -> None:
-        print(self.chans[21 : 21 + 15])
+        print(self.chans[21 : 21 + 15 * 3])
         if self.use_art_net:
             self.art_net_controller.show()
             return
