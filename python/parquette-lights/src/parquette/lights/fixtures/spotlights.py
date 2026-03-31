1535ef4f90aa10ba67c873151b71615651ad6e52
Severin Smith <severin@smallbutdigital.com>
2026-03-31 15:48:02 -0400

    Reset code

--- python/parquette-lights/src/parquette/lights/fixtures/spotlights.py
+++ python/parquette-lights/src/parquette/lights/fixtures/spotlights.py
@@ -413,7 +413,7 @@ class YRXY200Spot(Spot):
     def reset(self, reset: bool) -> None:
         value = YRXY200Spot.YRXY200Reset.NONE
         if reset:
-            value = YRXY200Spot.YRXY200Reset.XY_AXIS
+            value = YRXY200Spot.YRXY200Reset.LAMPS
 
         self.dmx.set_channel(
             self.addr + YRXY200Spot.YRXY200Channel.RESET.value, value.value
